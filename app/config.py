8085f917e867a0fe8a4459de82d34fc746cb1ab1
Santosh Narayanan <santosh33@gmail.com>
2024-09-09 00:38:05 +0200

    environment variable set

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
+import os
 from pydantic import Field
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+
+# UserWarning: CQLENG_ALLOW_SCHEMA_MANAGEMENT environment variable is not set.
+os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = "1"
 
 class Settings(BaseSettings):
     keyspace: str = Field(..., alias='ASTRADB_KEYSPACE')
