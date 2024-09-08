b8d427a2bd3920efe4b428f66aa766312c1ed9f4
Santosh Narayanan <santosh33@gmail.com>
2024-09-08 20:36:43 +0200

    Define Cassandra models

---
+++ app/users/models.py
@@ -0,0 +1,19 @@
+import uuid
+from app.config import get_settings
+import UUID
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import Model
+
+settings = get_settings()
+
+class User(Model):
+    __keyspace__ = settings.keyspace
+    email = columns.Text(primary_key=True)
+    user_id = columns.UUID(primary_key=True, default=uuid.uuid1)
+    password = columns.Text()
+
+    def __str__(self):
+        return self.__repr__()
+    
+    def __repr__(self):
+        return f"User(email={self.email}, user_id={self.user_id})"
