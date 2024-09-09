41a3415600be132094c2c141551c432e7aca4830
Santosh Narayanan <santosh33@gmail.com>
2024-09-09 14:46:50 +0200

    Validated email

--- app/users/models.py
+++ app/users/models.py
@@ -2,6 +2,7 @@ import uuid
 from app.config import get_settings
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
+from . import validators
 
 settings = get_settings()
 
@@ -9,10 +10,25 @@ class User(Model):
     __keyspace__ = settings.keyspace
     email = columns.Text(primary_key=True)
     user_id = columns.UUID(primary_key=True, default=uuid.uuid1)
-    password = columns.Text()
+    password = columns.Text()  # not secured
+
+    # confirmed = columns.UUID(blank=True)
 
     def __str__(self):
         return self.__repr__()
     
     def __repr__(self):
         return f"User(email={self.email}, user_id={self.user_id})"
+
+    @staticmethod
+    def create_user(email, password=None):
+        q = User.objects.filter(email=email)
+        if q.count() != 0:
+            raise Exception("User already has account.")
+        valid, msg, email = validators.validate_email(email)
+        if not valid:
+            raise Exception(f"Invalid email: {msg}")
+        obj = User(email=email)
+        obj.password = password
+        obj.save()
+        return obj
