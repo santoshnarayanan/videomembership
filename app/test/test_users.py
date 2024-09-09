8ca599f8f6082cf0db28b202b37325140fd3a680
Santosh Narayanan <santosh33@gmail.com>
2024-09-09 22:26:06 +0200

    Pytest

---
+++ app/test/test_users.py
@@ -0,0 +1,35 @@
+import pytest
+from app import db
+from app.users.models import User
+
+@pytest.fixture(scope="module")
+def setup():
+    # setup
+    session = db.get_session()
+    yield session
+    # teardown
+    q = User.objects.filter(email='test@test.com')
+    if q.count() != 0:
+        q.delete()
+    session.shutdown()
+
+def test_create_user(setup):
+    User.create_user("test@test.com", password='abc123')
+
+
+def test_duplicate_user(setup):
+    with pytest.raises(Exception) as e:
+        User.create_user("test@test.com", password='abc123')
+
+
+def test_invalid_email(setup):
+    with pytest.raises(Exception) as e:
+        User.create_user("test@test", password='abc123')
+
+
+def test_valid_password(setup):
+    q = User.objects.filter(email='test@test.com')
+    assert q.count() == 1
+    user_obj = q.first()
+    assert user_obj.verify_password('abc123') == True
+    assert user_obj.verify_password('abc1234') == False
