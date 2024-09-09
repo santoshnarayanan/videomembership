a0e49eeb428dff23f300c0d8ca176c7d0050d0d1
Santosh Narayanan <santosh33@gmail.com>
2024-09-09 14:47:09 +0200

    Validated email

---
+++ app/users/validators.py
@@ -0,0 +1,14 @@
+from email_validator import validate_email, EmailNotValidError
+
+
+def _validate_email(email):
+    msg = ""
+    valid = False
+    try:
+        valid = validate_email(email)
+        # update the email var with a normalized value
+        email = valid.email
+        valid = True
+    except EmailNotValidError as e:
+        msg = str(e)
+    return email, valid, msg
