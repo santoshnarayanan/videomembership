842d1de82ebb706e313981ab9b8f03d1593fbc3f
Santosh Narayanan <santosh33@gmail.com>
2024-09-10 14:36:52 +0200

    Forms SignUp and Login

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 import pathlib
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from  cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.management import sync_table
 
 from . import config, db
 from .users.models import User
 
 # BASE_DIR= pathlib.Path(__file__).resolve() # path of main.py
-BASE_DIR= pathlib.Path(__file__).resolve().parent # app directory
-TEMPLATE_DIR = BASE_DIR / "templates" # path of templates directory
+BASE_DIR = pathlib.Path(__file__).resolve().parent  # app directory
+TEMPLATE_DIR = BASE_DIR / "templates"  # path of templates directory
 
 app = FastAPI()
 templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
-DB_SESSION =None # Setting global variable
+DB_SESSION = None  # Setting global variable
 # settings = config.get_settings()
+
 
 @app.on_event("startup")
 def on_startup():
@@ -27,8 +28,34 @@ def on_startup():
 
 @app.get("/", response_class=HTMLResponse)
 def homepage(request: Request):
-    context = {"request": request, "abc":123}
+    context = {"request": request, "abc": 123}
     return templates.TemplateResponse("home.html", context)
+
+
+@app.get("/login", response_class=HTMLResponse)
+def login_get_view(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@app.post("/login", response_class=HTMLResponse)
+def login_post_view(request: Request, email: str = Form(...), password: str = Form(...)):
+    print(email, password)
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@app.get("/signup", response_class=HTMLResponse)
+def signup_get_view(request: Request):
+    return templates.TemplateResponse("auth/signup.html", {"request": request})
+
+
+@app.post("/signup", response_class=HTMLResponse)
+def signup_post_view(request: Request, email: str = Form(...), 
+                        password: str = Form(...), password_confirm: str = Form(...),
+                        
+                    ):
+    print(email, password)
+    return templates.TemplateResponse("auth/signup.html", {"request": request})
+
 
 
 @app.get("/users")
